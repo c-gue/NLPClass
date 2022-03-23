4f6f4082cddb3ed54591d7745b6b827b5dbfd7a2
Chris <c_guerra@baylor.edu>
2022-03-23 14:11:42 -0500

    language

--- nlp1.py
+++ nlp1.py
@@ -16,6 +16,36 @@ words = blob.words
 
 #print(blob.tags)
 
-print(blob.noun_phrases)
+#print(blob.noun_phrases)
+#Polarity is positive vs negative. Subjectivity is how subjective the phrase is.
+#Both are measured -1 to 1
+#print(blob.sentiment)
+#print("Polarity:",blob.sentiment.polarity)
+#print("Subjectivity:",blob.sentiment.subjectivity)
+#for sentence in sentences:
+    #print(sentence,':', round(sentence.sentiment.polarity,3))
+
+from textblob.sentiments import NaiveBayesAnalyzer
+
+blob = TextBlob(text, analyzer=NaiveBayesAnalyzer())
+
+print(blob.sentiment)
+
+for sentence in blob.sentences:
+    print(sentence.sentiment)
 
 
+spanish = blob.translate(to='es')
+print(spanish)
+
+chinese = blob.translate(to="zh")
+print(chinese)
+
+french = blob.translate(to='fr')
+print(french)
+
+hindi = blob.translate(to='ne')
+print(hindi)
+
+english = hindi.translate(to='en')
+print(english)
